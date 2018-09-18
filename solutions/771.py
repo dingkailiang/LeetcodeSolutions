0df8ed8358b5a4308e8ed10043683ae094939c75
Dingkai Liang <dingkailiang@gmail.com>
2018-09-18 17:02:17 -0600

    add 711

---
+++ solutions/771.py
@@ -0,0 +1,18 @@
+
+METHOD = "numJewelsInStones"
+
+TESTCASES = [("aA","aAAbbbb")]
+
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        count = 0
+        for stone in S:
+            if stone in J:
+                count += 1
+        return count
+        
