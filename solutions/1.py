ae8aa3f2565a1561891a02d839a52d1bd7046266
dingkailiang <dingkailiang@gmail.com>
2018-09-18 23:53:05 -0600

    add #1

--- solutions/1.py
+++ solutions/1.py
@@ -12,7 +12,10 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
+        mem = {}
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+            x = nums[i]
+            y = target - x
+            if y in mem:
+                return [i,mem[y]]
+            mem[x] = i
